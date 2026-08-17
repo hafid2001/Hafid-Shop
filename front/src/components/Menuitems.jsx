78aabe1cc894aec77c176de7913a634c6d28f350
matela <abdou26072001@gmail.com>
2026-08-17 13:36:48 +0100

    menu compontent

--- front/src/components/Menuitems.jsx
+++ front/src/components/Menuitems.jsx
@@ -1,5 +1,132 @@
-const Menuitems = () => {
-  return <div></div>;
+import {
+  FolderOpen,
+  Home,
+  ShoppingBag,
+  ShoppingCart,
+  User,
+  Mail,
+  Flashlight,
+} from "lucide-react";
+
+import { useNavigate , useLocation } from "react-router-dom";
+import { Link as ScrollLink } from "react-scroll";
+import { useState } from "react";
+
+
+export const menuItemsData = [
+  { to: "home", label: "Home", Icon: Home },
+  { to: "categories", label: "Categories", Icon: FolderOpen },
+  { to: "shop", label: "Shop", Icon: ShoppingBag },
+  { to: "contact", label: "Contact", Icon: Mail },
+];
+
+const Menuitems = ({ setSideBarOpen, isMobile }) => {
+  
+ const navigate = useNavigate();
+  const [cartItems] = useState({
+    item1: 2,
+    item2: 1,
+  });
+
+  const [token, setToken] = useState(true);
+  const location = useLocation();
+
+  const totalItems = Object.values(cartItems).reduce(
+    (a, b) => a + b,
+    0
+  );
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setToken(false);
+    navigate("/");
+    setSideBarOpen && setSideBarOpen(false);
+  };
+
+
+
+  return (
+    <div
+      className={`flex md:justify-center lg:justify-end ${
+        isMobile
+          ? "flex-col space-y-6 items-center px-4 gap-y-2"
+          : "flex-row w-full items-center gap-4"
+      }`}
+    >
+      {menuItemsData.map(({ to, label, Icon }) =>
+        location.pathname === "/" ? (
+          <ScrollLink
+            key={to}
+            to={to}
+            smooth={true}
+            duration={500}
+            offset={-80}
+            spy={true}
+            onClick={() => setSideBarOpen && setSideBarOpen(false)}
+            className="flex items-center gap-3 px-4 py-3 rounded-lg h-[35px] transition-all shrink-0 w-auto min-w-[80px] text-gray-200 hover:bg-white/10 hover:text-white hover:shadow-md cursor-pointer"
+            activeClass="bg-gradient-to-r from-indigo-500 to-pink-500 text-white shadow-lg"
+          >
+            <Icon className="w-6 h-6" />
+            <span className="font-semibold text-base">{label}</span>
+          </ScrollLink>
+        ) : (
+          <button
+            key={to}
+            onClick={() => {
+              navigate("/");
+           setSideBarOpen && setSideBarOpen(false); 
+            }}
+            className="flex items-center gap-3 px-4 py-3 rounded-lg h-[35px] transition-all shrink-0 w-auto min-w-[80px] text-gray-200 hover:bg-white/10 hover:text-white hover:shadow-md"
+          >
+            <Icon className="w-6 h-6" />
+            <span className="font-semibold text-base">{label}</span>
+          </button>
+        )
+      )}
+
+      {/* Cart */}
+      <button
+        onClick={() => {
+          navigate("/cart");
+        setSideBarOpen &&  setSideBarOpen(false);
+        }}
+        className="relative flex items-center gap-3 px-4 py-3 rounded-lg h-[35px] transition-all text-gray-200 hover:bg-white/10 hover:text-white hover:shadow-md"
+      >
+        <ShoppingCart className="w-6 h-6" />
+
+        {totalItems > 0 && (
+          <span className="absolute -top-1 -right-1 w-5 h-5 text-xs font-bold text-white bg-red-500 rounded-full flex items-center justify-center">
+            {totalItems}
+          </span>
+        )}
+      </button>
+
+      {/* Authentication  button login / logout  */}
+      {!token ? (
+        <button
+          onClick={() => {
+            navigate("/login");
+          setSideBarOpen && setSideBarOpen(false);
+          }}
+          className="flex items-center gap-2 px-4 py-3 rounded-lg h-[35px] bg-cyan-400 text-white font-semibold hover:bg-cyan-500 transition-all"
+        >
+        
+          Login
+        </button>
+      ) : (
+        <div className="flex items-center gap-4">
+          <User className="w-6 h-6 text-white" />
+
+          <button
+            onClick={handleLogout}
+            className="flex items-center gap-2 px-4 py-3 rounded-lg h-[35px] bg-red-500 text-white font-semibold hover:bg-red-600 transition-all"
+          >
+            Log Out
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Menuitems;
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,11 +1,38 @@
-
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import Categories from "../components/Categories";
+import Offer from "../components/Offer";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      {/* قسم الهوم */}
+      <section id="home">
+        <Hero />
+      </section>
 
-export default Home
+      {/* قسم المميزات */}
+      <section id="features">
+        <Features />
+      </section>
+
+      {/* قسم التصنيفات */}
+      <section id="categories">
+        <Categories />
+      </section>
+
+      {/* قسم العروض */}
+      <section id="shop">
+        <Offer />
+      </section>
+
+      {/* قسم التواصل */}
+      <section id="contact">
+        <Footer />
+      </section>
+    </div>
+  );
+};
+
+export default Home;
